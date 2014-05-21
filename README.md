ec80620d496fe864409a4bf3914751397afa25ed
Michael van der Weg <michael@joinbox.com>
2014-05-21 21:22:17 +0200

    first release

---
+++ README.md
@@ -0,0 +1,29 @@
+# ee-soa-rpc-request
+
+Easy SOA requests
+
+## installation
+
+	npm install ee-soa-rpc-request
+
+## build status
+
+[![Build Status](https://travis-ci.org/eventEmitter/ee-soa-rpc-request.png?branch=master)](https://travis-ci.org/eventEmitter/ee-soa-rpc-request)
+
+
+## usage
+
+
+	var rpcRequest = require('ee-soa-rpc-request');
+
+	// you have to bind the rpcRequest class to any object accepting 
+	// requets via their request event
+	var RPCRequest = rpcRequest(website);
+
+
+	myRequest = new RPCRequest({
+		  select: '*, eventdata.*, eventdata.tag.*'
+		, filter: 'name=3, eventdata.tag.name=>"hui"'
+	}).send('event', function(status, data) {
+
+	});
---
+++ test/default.js
@@ -0,0 +1,61 @@
+
+	
+	var   Class 		= require('ee-class')
+		, log 			= require('ee-log')
+		, EventEmitter 	= require('ee-event-emitter')
+		, assert 		= require('assert');
+
+
+	var Target = new Class({inherits: EventEmitter})
+		target = new Target();
+
+
+	var   Request  		= require('../')	
+		, EasyRequest  	= Request(target)
+		, req;
+
+
+
+	describe('The Request', function(){
+		it('should not throw when instantiated', function(){
+			req = new EasyRequest();
+		});
+
+		it('should parse a simple filter statement', function(){
+			req = new EasyRequest({
+				filter: 'name=3'
+			});
+
+			assert.equal(JSON.stringify(req.filters), '{\"name\":[{\"operator\":\"=\",\"value\":\"3\"}]}');
+		});	
+
+		it('should parse an advanced filter statement', function(){
+			req = new EasyRequest({
+				filter: 'name=3, eventdata.tag.name=>"hui"'
+			});
+
+			assert.equal(JSON.stringify(req.filters), '{"name":[{"operator":"=","value":"3"}],"eventdata":{"tag":{"name":[{"operator":"=>","value":"hui"}]}}}');
+		});
+
+		it('should parse a select statement', function(){
+			req = new EasyRequest({
+				select: '*, eventdata.* eventdata.tag.*'
+			});
+		});
+
+		it('emit the request event with the corect configured requests', function(done){
+			target.on('request', function(req, res){
+				assert.equal(JSON.stringify(req), '{"formats":{},"filters":{"name":[{"operator":"=","value":"3"}],"eventdata":{"tag":{"name":[{"operator":"=>","value":"hui"}]}}},"relatedTo":{"model":null,"id":null},"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"fields":"*","collection":"event","subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"1":{"type":"application","subtype":"json"},"length":2},"filters":{},"relatedTo":{"model":null,"id":null},"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"collection":"eventdata","version":"0.0.1","action":1,"fields":"*","subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"collection":"tag","version":"0.0.1","action":1,"fields":"*"}]}]}');
+				res.send(1, {});
+			});
+
+			req = new EasyRequest({
+				  select: '*, eventdata.*, eventdata.tag.*'
+				, filter: 'name=3, eventdata.tag.name=>"hui"'
+			}).send('event', function(status, data){
+				assert.equal(status, 1);
+				done();
+			});
+		});
+	});
+	
