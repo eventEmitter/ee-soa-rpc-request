a7041467617fa8468e4b6f920fc8c5bf9f79274a
tobiaskneubuehler <tobias@joinbox.com>
2015-04-06 21:36:46 +0200

    tests update

--- test/default.js
+++ test/default.js
@@ -21,78 +21,94 @@
 			req = new EasyRequest();
 		});
 
-		it('should parse a simple filter statement', function(){
+		it('should parse a simple filter statement', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req.filters), '{\"name\":[{\"operator\":\"=\",\"value\":3}]}');
+				done();
+			});
+
 			req = new EasyRequest({
 				filter: 'name=3'
-			});
-
-			assert.equal(JSON.stringify(req.filters), '{\"name\":[{\"operator\":\"=\",\"value\":\"3\"}]}');
+			}).send();
 		});
 
-		it('should parse an advanced filter statement', function(){
+		it('should parse an advanced filter statement', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req.filters), '{"name":[{"operator":"=","value":3}],"eventdata":{"tag":{"name":[{"operator":"=","value":"hui"}]}}}');
+				done();
+			});
+
 			req = new EasyRequest({
-				filter: 'name=3, eventdata.tag.name=>"hui"'
-			});
-
-			assert.equal(JSON.stringify(req.filters), '{"name":[{"operator":"=","value":"3"}],"eventdata":{"tag":{"name":[{"operator":"=>","value":"hui"}]}}}');
+				filter: 'name=3, eventdata.tag.name="hui"'
+			}).send();
 		});
 
+		it('should parse a select statement', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req), '{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"undefined","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"eventdata","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"tag","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[]}]}]}');
+				done();
+			});
 
-
-		it('should parse a select statement', function(){
 			req = new EasyRequest({
 				select: '*, eventdata.*, eventdata.tag.*'
-			});
-			req._collectSubRequests(req.requests);
-
-			assert.equal(JSON.stringify(req._request), '{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*","eventdata"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"languages":["en"],"version":"0.0.1","action":1,"subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"1":{"type":"application","subtype":"json"},"length":2},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*","tag"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"collection":"eventdata","languages":["en"],"version":"0.0.1","action":1,"subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"collection":"tag","languages":["en"],"version":"0.0.1","action":1,"subRequests":[]}]}]}');
+			}).send();
+			
 		});
 
+		it('should parse a complex select statement', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req), '{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"undefined","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"eventdata","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"venue","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[]},{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"tag","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[]}]}]}');
+				done();
+			});
 
-
-		it('should parse a complex select statement', function(){
 			req = new EasyRequest({
 				select: '*, eventdata.*, eventdata.venue.*, eventdata.tag.*'
-			});
-			req._collectSubRequests(req.requests);
-
-			assert.equal(JSON.stringify(req._request), '{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*","eventdata"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"languages":["en"],"version":"0.0.1","action":1,"subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"1":{"type":"application","subtype":"json"},"2":{"type":"application","subtype":"json"},"length":3},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*","venue","tag"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"collection":"eventdata","languages":["en"],"version":"0.0.1","action":1,"subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"collection":"venue","languages":["en"],"version":"0.0.1","action":1,"subRequests":[]},{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"collection":"tag","languages":["en"],"version":"0.0.1","action":1,"subRequests":[]}]}]}');
+			}).send();
+			
 		});
 
+		it('should parse a range statement', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req.range), '{"from":0,"to":10}');
+				done();
+			});
 
-
-		it('should parse a range statement', function(){
 			req = new EasyRequest({
 				range: '0-10'
-			});
-			assert.equal(JSON.stringify(req._request.range), '{"from":0,"to":10}');
+			}).send();
 		});
 
-		it('should parse a simple order statement', function(){
+		it('should parse a simple order statement', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req.order), '{"startdate":"DESC"}');
+				done();
+			});
+
 			req = new EasyRequest({
 				order: 'startdate DESC'
-			});
-			assert.equal(JSON.stringify(req._request.order), '{"startdate":"DESC"}');
+			}).send();
 		});
 
-		it('should parse an order statement', function(){
+		it('should parse an order statement', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req.order), '{"startdate":"DESC","eventData":{"id":"ASC","venue":{"id":"ASC"}}}');
+				done();
+			});
+
 			req = new EasyRequest({
 				order: 'startdate DESC, eventData.id, eventData.venue.id'
-			});
-			assert.equal(JSON.stringify(req._request.order), '{"startdate":"DESC","eventData":{"id":"ASC","venue":{"id":"ASC"}}}');
+			}).send();
 		});
 
-
-
-		it('emit the request event with the corect configured requests', function(done){
-			target.on('request', function(req, res){
-				assert.equal(JSON.stringify(req), '{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{"name":[{"operator":"=","value":"3"}],"eventdata":{"tag":{"name":[{"operator":"=>","value":"hui"}]}}},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*","eventdata"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"languages":["en"],"version":"0.0.1","action":1,"collection":"event","subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"1":{"type":"application","subtype":"json"},"length":2},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*","tag"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"collection":"eventdata","languages":["en"],"version":"0.0.1","action":1,"subRequests":[{"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"collection":"tag","languages":["en"],"version":"0.0.1","action":1,"subRequests":[]}]}]}');
+		it('emit the request event with the correct configured requests', function(done){
+			target.off().on('request', function(req, res){
+				assert.equal(JSON.stringify(req), '{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{"name":[{"operator":"=","value":3}],"eventdata":{"tag":{"name":[{"operator":"=","value":"hui"}]}}},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"event","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"eventdata","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[{"action":1,"formats":{"0":{"type":"application","subtype":"json"},"length":1},"filters":{},"relatedTo":{"model":null,"id":null},"options":{},"parameters":{},"fields":["*"],"accessToken":{"type":null,"value":null},"requestToken":{"type":null,"value":null},"range":{"from":0,"to":null},"version":"0.0.1","collection":"tag","resourceId":null,"languages":["en"],"contentType":null,"subRequests":[]}]}]}');
 				res.send(1, {});
 			});
 
 			req = new EasyRequest({
 				  select: '*, eventdata.*, eventdata.tag.*'
-				, filter: 'name=3, eventdata.tag.name=>"hui"'
+				, filter: 'name=3, eventdata.tag.name="hui"'
 			}).send('event', function(status, data){
 				assert.equal(status, 1);
 				done();
